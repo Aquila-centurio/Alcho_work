6ae6246f63b0eb0248190c8f46f5446121ef33f7
Aquila-centurio <misterkirillilinych@gmail.com>
2024-04-19 18:09:55 +0500

    Снова вынос методов

--- main.cpp
+++ main.cpp
@@ -1,36 +1,8 @@
 #include <iostream>
 #include "classes.h"
+#include "methods.h"
 //#include "methods.h"
 using namespace std;   
-
-void Wine::displayInfo() 
-{
-    cout << "Wine: " << name << ", Alcohol: " << alcoholContent << "%, Volume: " << volume << "L, Price: $" << price << ", Grape: " << grapeSort << ", Country: " << country << endl;
-}
-
-float Wine::calculateCost(int quantity) 
-{
-    return price * quantity;
-}
-
-void Beer::displayInfo() 
-{
-    cout << "Beer: " << name << ", Alcohol: " << alcoholContent << "%, Volume: " << volume << "L, Price: $" << price << ", Type: " << type << ", Manufacturer: " << manufacturer << endl;
-}
-float Beer::calculateCost(int quantity) 
-{
-    return price * quantity;
-}
-
-void Vodka::displayInfo() 
-{
-    cout << "Vodka: " << name << ", Alcohol: " << alcoholContent << "%, Volume: " << volume << "L, Price: $" << price << ", Country: " << countryOfOrigin << ", Composition: " << composition << endl;
-}
-
-float Vodka::calculateCost(int quantity)
-{
-    return price * quantity;
-}
 
 
 int main(int, char**){
