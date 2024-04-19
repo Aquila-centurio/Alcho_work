5462b3ee18acc0180fe2d6154b6bc03428d08be7
Aquila-centurio <misterkirillilinych@gmail.com>
2024-04-19 07:52:45 +0500

    Добавление контейнеров

--- classes.h
+++ classes.h
@@ -2,7 +2,7 @@
 #define MainH
 #include <iostream>
 #include <string>
-
+#include <vector>
 
 using namespace std;
 
@@ -84,6 +84,135 @@ public:
 
     void displayInfo() override;
     float calculateCost(int quantity) override;
+};
+
+
+// ------------ КОНТЕЙНЕРЫ ----------------
+
+// Базовый класс для контейнеров
+class AlcoholContainer {
+public:
+    virtual ~AlcoholContainer() {} 
+    virtual void addDrink(AlcoholDrink* drink) = 0;
+    virtual void removeDrink(const string& drinkName) = 0; 
+    virtual void displayContents() const = 0; 
+    virtual AlcoholDrink* findDrink(const string& drinkName) const = 0; 
+};
+
+// Контейнер на основе массива
+class ArrayAlcohol : public AlcoholContainer {
+private:
+    vector<AlcoholDrink*> drinks; 
+public:
+    void addDrink(AlcoholDrink* drink) override {
+        drinks.push_back(drink); 
+    }
+
+    void removeDrink(const string& drinkName) override {
+       
+        for (auto it = drinks.begin(); it != drinks.end(); ++it) {
+            if ((*it)->getName() == drinkName) {
+                delete *it; 
+                drinks.erase(it); 
+                break;
+            }
+        }
+    }
+
+    void displayContents() const override {
+        // Отображение информации о каждом напитке
+        for (const auto& drink : drinks) {
+            drink->displayInfo();
+        }
+    }
+
+    AlcoholDrink* findDrink(const string& drinkName) const override {
+        // Поиск напитка по имени
+        for (const auto& drink : drinks) {
+            if (drink->getName() == drinkName) {
+                return drink;
+            }
+        }
+        return nullptr; // Напиток не найден
+    }
+};
+
+// Контейнер на основе связанного списка
+class ListAlcohol : public AlcoholContainer {
+private:
+    struct Node {
+        AlcoholDrink* drink;
+        Node* next;
+        Node(AlcoholDrink* _drink) : drink(_drink), next(nullptr) {} // Конструктор узла
+    };
+    Node* head; // Указатель на начало списка
+
+public:
+    ListAlcohol() : head(nullptr) {} // Конструктор
+
+    void addDrink(AlcoholDrink* drink) override {
+        // При пустом списке оздаем новый узел, при имеющимся перемещаемся в конец
+        if (!head) 
+        {
+            head = new Node(drink); 
+        } 
+        else 
+        {
+            Node* temp = head;
+            while (temp->next) {
+                temp = temp->next; 
+        }
+            temp->next = new Node(drink); 
+        }
+    }
+
+    void removeDrink(const string& drinkName) override {
+        Node* curr = head;
+        Node* prev = nullptr;
+        while (curr) {
+            if (curr->drink->getName() == drinkName) {
+                if (prev) {
+                    prev->next = curr->next; 
+                } else {
+                    head = curr->next; 
+                }
+                delete curr->drink;
+                delete curr; 
+                break;
+            }
+            prev = curr;
+            curr = curr->next;
+        }
+    }
+
+    void displayContents() const override {
+        Node* temp = head;
+        while (temp) {
+            temp->drink->displayInfo(); 
+            temp = temp->next; 
+        }
+    }
+
+    AlcoholDrink* findDrink(const string& drinkName) const override {
+        Node* temp = head;
+        while (temp) {
+            if (temp->drink->getName() == drinkName) {
+                return temp->drink; 
+            }
+            temp = temp->next;
+        }
+        return nullptr; 
+    }
+
+    ~ListAlcohol() { // Деструктор для узлов
+        Node* temp = head;
+        while (temp) {
+            Node* next = temp->next; 
+            delete temp->drink;
+            delete temp; 
+            temp = next; 
+        }
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 #include "classes.h"
-#include "methods.h"
+//#include "methods.h"
 using namespace std;   
 
+void Wine::displayInfo() 
+{
+    cout << "Wine: " << name << ", Alcohol: " << alcoholContent << "%, Volume: " << volume << "L, Price: $" << price << ", Grape: " << grapeSort << ", Country: " << country << endl;
+}
+
+float Wine::calculateCost(int quantity) 
+{
+    return price * quantity;
+}
+
+void Beer::displayInfo() 
+{
+    cout << "Beer: " << name << ", Alcohol: " << alcoholContent << "%, Volume: " << volume << "L, Price: $" << price << ", Type: " << type << ", Manufacturer: " << manufacturer << endl;
+}
+float Beer::calculateCost(int quantity) 
+{
+    return price * quantity;
+}
+
+void Vodka::displayInfo() 
+{
+    cout << "Vodka: " << name << ", Alcohol: " << alcoholContent << "%, Volume: " << volume << "L, Price: $" << price << ", Country: " << countryOfOrigin << ", Composition: " << composition << endl;
+}
+
+float Vodka::calculateCost(int quantity)
+{
+    return price * quantity;
+}
 
 
 int main(int, char**){
     std::cout << "Hello, from Alchogol_work!\n";
+    // Создание объектов алкогольных напитков
+    Wine wine("Merlot", 12.5, 0.75, 20.0, "Merlot", "France");
+    Beer beer("Heineken", 5.0, 0.5, 3.0, "Lager", "Netherlands");
+    Vodka vodka("Absolut", 40.0, 1.0, 25.0, "Sweden", "Potatoes");
+    
 }
